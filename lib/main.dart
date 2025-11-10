286225a4bafff525c8c408921b6156c380ff413a
zAldajan <z.aldajan@qu.edu.sa>
2025-11-10 11:53:28 +0300

    Refacoration

--- lib/main.dart
+++ lib/main.dart
@@ -39,13 +39,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
 
           // ↓ Add Button.
           ElevatedButton(
@@ -57,6 +57,23 @@ class MyHomePage extends StatelessWidget {
 
         ],
       ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Text(pair.asLowerCase),
     );
   }
 }
